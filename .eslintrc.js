706ea82620733d955c9a9e3901e5cb894f0155d6
uknmr <y.kinmori@gmail.com>
2022-05-18 16:19:51 +0900

    🔧: mdx 用 eslint 設定を追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-  extends: 'smarthr',
+  extends: ['smarthr'],
+  overrides: [
+    {
+      files: '*.mdx',
+      extends: ['smarthr', 'plugin:mdx/recommended'],
+      rules: {
+        'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx', '.mdx'] }],
+      },
+    },
+  ],
 }
