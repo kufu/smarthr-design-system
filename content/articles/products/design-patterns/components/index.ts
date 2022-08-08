3f24f726cbb0d12f61f6ea1c0d5f83d3034bf8b7
Yu Watanabe <watanabe.yu.1203@gmail.com>
2022-08-08 15:01:58 +0900

    上に戻るリンクのコード例をStoryにする

--- content/articles/products/design-patterns/components/index.ts
+++ content/articles/products/design-patterns/components/index.ts
@@ -1,3 +1,4 @@
 import SelectCompanyAccount from '!!raw-loader!../select-company-account/SelectCompanyAccount'
+import UpwardNavigation from '!!raw-loader!../upward-navigation/UpwardNavigation'
 
-export { SelectCompanyAccount }
+export { SelectCompanyAccount, UpwardNavigation }
