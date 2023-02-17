a8345e54d1bb8e31bbf3480835d0966ec9e2847c
Yu Watanabe <watanabe.yu.1203@gmail.com>
2023-02-17 18:03:05 +0900

    OGP画像用のタイトルが<Head>コンポーネントに渡せていなかったので修正

--- src/templates/article.tsx
+++ src/templates/article.tsx
@@ -376,7 +376,7 @@ export const Head: FC<Props> = ({ data }) => {
   // memo: カテゴリのtitleとカテゴリ直下のindexページのタイトルが重複した場合はカテゴリ名のみを表示する
   const headTitle = title === parentCategoryName ? title : `${title} | ${parentCategoryName}`
 
-  return <HeadComponent title={headTitle} description={description} />
+  return <HeadComponent title={headTitle} description={description} ogTitle={title} />
 }
 
 const Wrapper = styled.div`
