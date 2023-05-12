0664f58d095e9597602e259c4282033f450a63d6
tomof <fffjt007@gmail.com>
2023-05-12 09:45:50 +0900

    Rangeの指定が逆だったので修正

--- src/components/ComponentPreview/ProductWrapper.tsx
+++ src/components/ComponentPreview/ProductWrapper.tsx
@@ -67,16 +67,16 @@ const padding = css(
   ({ theme: { space } }) => css`
     padding-inline: ${space(1.5)};
 
-    @media (width >= 1440px) {
+    @media (width <= 1440px) {
       padding-inline: ${space(1.25)};
     }
-    @media (width >= 1024px) {
+    @media (width <= 1024px) {
       padding-inline: ${space(1)};
     }
-    @media (width >= 768px) {
+    @media (width <= 768px) {
       padding-inline: ${space(0.75)};
     }
-    @media (width >= 480px) {
+    @media (width <= 480px) {
       padding-inline: ${space(0.5)};
     }
   `,
