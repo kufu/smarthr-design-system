14d2acf0d64c5821f1f8dd2806c97c45aefd4a2c
tomof <fffjt007@gmail.com>
2023-06-12 11:41:57 +0900

    2階層あるコンポーネントの考慮がなかったので修正

--- plugins/gatsby-source-component-captures/fetchComponentCaptures.ts
+++ plugins/gatsby-source-component-captures/fetchComponentCaptures.ts
@@ -2,6 +2,7 @@ const STORYBOOK_URL = 'https://story.smarthr-ui.dev'
 
 type Story = {
   kind: string
+  importPath: string
   tags: string[]
 }
 
@@ -10,12 +11,34 @@ type StoryKind = {
   iframeUrl: string
   thumbnailFileName: string
   displayName: string
+  componentPath: string
   numberOfStories: number
 }
 
 export type StoryGroup = {
   groupName: string
   storyKinds: StoryKind[]
+}
+
+const convertKebab = (target: string) => {
+  return target
+    .replace('SmartHR', 'smarthr') // 特殊なケース
+    .replace(/[^a-zA-Z0-9-]/g, '') // 全角文字などの半角英数字以外を除去
+    .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+    .replace(/([A-Z])([A-Z][a-z])/g, '$1-$2')
+    .toLowerCase()
+}
+
+const convertComponentPath = (importPath: string, displayName: string) => {
+  const matches = importPath.match(/\.\/src\/components\/(.*)\.stories\.tsx/)
+  if (!matches) return ''
+  const componentDirPath = matches[1]
+    .split('/')
+    .slice(0, -2)
+    .map((item) => convertKebab(item))
+    .join('/')
+  const componentPathName = convertKebab(displayName)
+  return componentDirPath === '' ? componentPathName : `${componentDirPath}/${componentPathName}`
 }
 
 export const fetchComponentCaptures = async () => {
@@ -25,13 +48,14 @@ export const fetchComponentCaptures = async () => {
 
   const storyGroups: StoryGroup[] = []
   Object.keys(storiesMap).forEach((id) => {
-    const { kind, tags } = storiesMap[id]
+    const { kind, tags, importPath } = storiesMap[id]
     if (tags.includes('docs')) return // ドキュメントはコンポーネント一覧として表示しない
 
     const groupName = kind.split('/')[0]
     const displayName = kind.split('/')[1]
-    const iframeUrl = `${STORYBOOK_URL}/iframe.html?id=${encodeURIComponent(id)}&viewMode=story&shortcuts=false&singleStory=true`
     const thumbnailFileName = `${groupName}-${displayName}.png`
+    const iframeUrl = `${STORYBOOK_URL}/iframe.html?id=${encodeURIComponent(id)}&viewMode=story&shortcuts=false&singleStory=true`
+    const componentPath = convertComponentPath(importPath, displayName)
 
     // Groupが存在しない場合は新規作成
     const storyGroup = storyGroups.find((item) => item.groupName === groupName)
@@ -44,6 +68,7 @@ export const fetchComponentCaptures = async () => {
             iframeUrl,
             thumbnailFileName,
             displayName,
+            componentPath,
             numberOfStories: 1,
           },
         ],
@@ -59,6 +84,7 @@ export const fetchComponentCaptures = async () => {
         iframeUrl,
         thumbnailFileName,
         displayName,
+        componentPath,
         numberOfStories: 1,
       })
       return
