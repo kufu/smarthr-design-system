c3b5bca9ad0a2cea0d3a32b1a79c064274e375cb
Yu Watanabe <watanabe.yu.1203@gmail.com>
2023-08-22 16:17:29 +0900

    新しいログインの方式に対応

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,5 +1,6 @@
-import { CSS_COLOR, CSS_FONT_SIZE, CSS_SIZE } from '@Constants/style'
+import { CSS_COLOR, CSS_SIZE } from '@Constants/style'
 import { LoginContext, LoginStatusKey } from '@Context/LoginContext'
+import { navigate } from 'gatsby'
 import React, { FC, useContext, useState } from 'react'
 import { Button, FaLockIcon, Input } from 'smarthr-ui'
 import styled from 'styled-components'
@@ -13,29 +14,15 @@ export const LoginPage: FC = () => {
   return (
     <Wrapper>
       <h1>従業員ログイン</h1>
-      <MaintenanceText>現在ログインできません。システム復旧中です。</MaintenanceText>
-      <p>限定コンテンツを利用したい場合、次の2つの方法で利用できます。（社内限定）</p>
+      <p>ログインすると限定コンテンツにアクセスできます。パスワードの確認方法は2つあります。</p>
       <ul>
         <li>
-          <p>
-            社内Slack<code>#design_system_相談</code>で利用したい限定コンテンツを伝える
-          </p>
+          <p>SmartHR社の1Passwordを利用する</p>
         </li>
         <li>
-          <p>
-            GitHubリポジトリを確認する（
-            <a href="https://github.com/kufu/smarthr-design-system-private" target="_blank" rel="noreferrer">
-              https://github.com/kufu/smarthr-design-system-private
-            </a>
-            ）
-          </p>
+          <p>SmartHR社のSlackに「SDSパスワード」と入力する（自動レスポンスがあります）</p>
         </li>
       </ul>
-      <p>
-        そのほか相談、問い合わせ先
-        <br />
-        社内Slack<code>#design_system_相談</code>
-      </p>
       <div className="inputs">
         <Input
           type="password"
@@ -44,14 +31,12 @@ export const LoginPage: FC = () => {
           onChange={(e) => setPassword(e.currentTarget.value)}
           value={password}
           name="password"
-          disabled={true}
         />
 
         <Button
           variant="primary"
           wide={true}
           onClick={() => login(password, () => setPassword(''), setErrMessage, updateLoginStatus)}
-          disabled={true}
         >
           ログイン
         </Button>
@@ -66,41 +51,39 @@ type Login = (
   password: string,
   clearInput: () => void,
   setErrMessage: (message: string) => void,
-  updateLoginStatus: (newStatus: LoginStatusKey) => void,
+  updateLoginStatus: (newStatus: LoginStatusKey, newPassword: string) => void,
 ) => Promise<void>
 const login: Login = async (password, clearInput, setErrMessage, updateLoginStatus) => {
-  const formDataString = `password=${password}`
-
   clearInput()
   setErrMessage('')
 
   const res = await fetch('/private', {
-    method: 'POST',
-    body: formDataString,
+    method: 'HEAD',
     redirect: 'manual',
     headers: {
-      'content-type': 'application/x-www-form-urlencoded',
+      'Sds-Private-Auth': password,
     },
   })
 
-  // ログインが成功し、Cookieが発行され、リダイレクトされる場合、statusは0になる。
-  // https://fetch.spec.whatwg.org/#concept-filtered-response-opaque-redirect
-
-  if (res.status === 0) {
-    // ログインに成功したら前のページに戻る
-    updateLoginStatus('loggedIn')
-    history.back()
+  // サイトルートには何も置いてないので、パスワードが合っていると404が返る
+  if (res.status === 404) {
+    // ログインに成功したらパスワードを保存し前のページに戻る
+    updateLoginStatus('loggedIn', password)
+    let prevPath = '/'
+    try {
+      const previousUrl = new URL(document.referrer)
+      if (previousUrl.hostname === window.location.hostname) {
+        prevPath = previousUrl.pathname
+      }
+    } catch (error) {
+      // 前のページがない場合はトップに戻る
+    }
+    navigate(prevPath) // history.back()だとcontextが失われるのでnavigateを使う
     return
 
-    // ログイン済みだとPOSTではコンテンツがとれないので404になる。
-  } else if (res.status === 404) {
-    updateLoginStatus('loggedIn')
-    setErrMessage('ログイン済みです。')
-    return
-
-    // パスワードが違うとパスワード画面が401で返される
+    // パスワードが違うと401が返る
   } else if (res.status === 401) {
-    updateLoginStatus('loggedOut')
+    updateLoginStatus('loggedOut', '')
     setErrMessage('ログインに失敗しました')
     return
 
@@ -110,8 +93,6 @@ const login: Login = async (password, clearInput, setErrMessage, updateLoginStat
     return
   }
 }
-
-const MaintenanceText = styled.p``
 
 const Wrapper = styled.div`
   box-sizing: border-box;
@@ -146,22 +127,10 @@ const Wrapper = styled.div`
     text-align: center;
   }
 
-  & ${MaintenanceText} {
-    color: ${CSS_COLOR.DANGER};
-  }
-
   & p {
     padding: 0;
     margin: 0;
     color: ${CSS_COLOR.TEXT_BLACK};
     line-height: 1.6;
-    & code {
-      padding: 0.125rem 0.25rem;
-      border-radius: 4px;
-      background-color: ${CSS_COLOR.LIGHT_GREY_2};
-      font-size: ${CSS_FONT_SIZE.PX_14};
-      vertical-align: 0.05rem;
-      margin: 0.125rem;
-    }
   }
 `
