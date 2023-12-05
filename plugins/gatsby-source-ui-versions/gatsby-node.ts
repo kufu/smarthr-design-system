cc9e99607b54b100e93d6cc9bb3268ba43dd66fd
Yu Watanabe <watanabe.yu.1203@gmail.com>
2023-12-05 10:37:46 +0900

    ビルド時のCSSコピーは不要なので削除

--- plugins/gatsby-source-ui-versions/gatsby-node.ts
+++ plugins/gatsby-source-ui-versions/gatsby-node.ts
@@ -1,23 +1,8 @@
-import fs from 'fs'
-import path from 'path'
-
 import { SourceNodesArgs } from 'gatsby'
 
 import { fetchUiVersions } from './fetchUiVersions'
 
 const NODE_TYPE = `UiVersion`
-
-exports.onPreInit = () => {
-  try {
-    fs.copyFileSync(
-      path.join(process.cwd(), './node_modules/smarthr-ui/smarthr-ui.css'),
-      path.join(process.cwd(), './static/smarthr-ui.css'),
-    )
-    console.log('smarthr-ui.css をコピーしました!!')
-  } catch (error) {
-    console.error('Error copy smarthr-ui.css from node_modules:', error)
-  }
-}
 
 exports.sourceNodes = async ({ actions, createContentDigest, createNodeId }: SourceNodesArgs) => {
   const { createNode } = actions
