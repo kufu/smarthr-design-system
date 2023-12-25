ce60daee2d3140b71c71b6d34ac9930d766b279d
Yu Watanabe <watanabe.yu.1203@gmail.com>
2023-12-25 10:54:39 +0900

    LiveEditorのエディタをiframeの外に移動

--- src/components/article/CodeBlock/LiveContainer.tsx
+++ src/components/article/CodeBlock/LiveContainer.tsx
@@ -1,11 +1,12 @@
 import { CSS_COLOR } from '@Constants/style'
 import { Script } from 'gatsby'
 import { themes } from 'prism-react-renderer'
-import React, { FC } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
+import Frame, { FrameContextConsumer } from 'react-frame-component'
 import { LiveEditor, LiveError, LivePreview, LiveProvider } from 'react-live'
 import { CssBaseLine } from 'smarthr-normalize-css'
 import * as ui from 'smarthr-ui'
-import styled, { ThemeProvider, css } from 'styled-components'
+import styled, { StyleSheetManager, ThemeProvider, css } from 'styled-components'
 import 'smarthr-ui/smarthr-ui.css'
 
 import { ComponentPreview } from '../../ComponentPreview'
@@ -15,10 +16,7 @@ import { CopyButton } from './CopyButton'
 
 import type { LiveContainerProps } from './CodeBlock'
 
-type Props = {
-  tsLoaded: boolean
-  setTsLoaded: React.Dispatch<React.SetStateAction<boolean>>
-} & LiveContainerProps
+type Props = LiveContainerProps
 
 const smarthrTheme = ui.createTheme()
 
@@ -33,54 +31,94 @@ const transformCode = (snippet: string) => {
   })
 }
 
-export const LiveContainer: FC<Props> = ({
-  code,
-  language,
-  scope,
-  noIframe,
-  withStyled,
-  gap,
-  align,
-  layout,
-  tsLoaded,
-  setTsLoaded,
-}) => (
-  <ThemeProvider theme={smarthrTheme}>
-    {/* ライブエディタ内のコードのトランスパイルに使用するTS（容量が大きいためCDNを利用） */}
-    <Script src="https://unpkg.com/typescript@latest/lib/typescript.js" onLoad={() => setTsLoaded(true)} />
-    {tsLoaded && (
-      <LiveProvider
-        code={code}
-        language={language}
-        scope={{ ...React, ...ui, styled, css, ...scope }}
-        theme={{
-          ...themes.vsDark,
-          plain: {
-            color: CSS_COLOR.LIGHT_GREY_3,
-            backgroundColor: CSS_COLOR.TEXT_BLACK,
-          },
-        }}
-        noInline={withStyled}
-        transformCode={transformCode}
-      >
-        <ComponentPreview gap={gap} align={align} layout={layout}>
-          {!noIframe && <CssBaseLine />}
-          <LivePreview Component={React.Fragment} />
-        </ComponentPreview>
-        <CodeWrapper>
-          <StyledLiveEditorContainer>
-            <PreContainer>
-              <CopyButton text={code || ''} />
-              {/* @ts-ignore -- LiveEditorの型定義が正しくないようなので、エラーを無視。 https://github.com/FormidableLabs/react-live/pull/234 */}
-              <LiveEditor padding={0} />
-            </PreContainer>
-          </StyledLiveEditorContainer>
-        </CodeWrapper>
-        <LiveError />
-      </LiveProvider>
-    )}
-  </ThemeProvider>
-)
+export const LiveContainer: FC<Props> = ({ code, language, scope, noIframe, withStyled, gap, align, layout }) => {
+  const [tsLoaded, setTsLoaded] = useState(false)
+
+  const iframeRef = useRef<HTMLIFrameElement>(null)
+  const [iframeHeight, setIframeHeight] = useState(600) // デフォルトの高さを設定
+
+  // Gatsbyではページロード時に<Frame>がレンダリングされないため、クライアントで表示をトリガーする
+  // https://github.com/ryanseddon/react-frame-component/issues/192#issuecomment-1153078390
+  const [showFrame, setShowFrame] = useState(false)
+  useEffect(() => {
+    setShowFrame(true)
+  }, [])
+
+  // iframeの高さをコンテンツに合わせて変更する
+  useEffect(() => {
+    if (!tsLoaded) return // CDNからのTSスクリプトのロード後に描画されるので、それまでは高さを計算しない
+    const innerWindow = iframeRef.current?.contentWindow
+    if (!innerWindow) return // ここに該当することはないはず
+
+    // TSスクリプトロード後、レンダリングが終わるのを待ってから高さを計算・セットする
+    setTimeout(() => {
+      const height = innerWindow.document.body.scrollHeight
+      if (height > 0) {
+        setIframeHeight(height + 8) // ComponentPreviewコンポーネントに`margin-block-start: 8px`が指定されているため
+      }
+    }, 500)
+  }, [tsLoaded])
+
+  return (
+    <ThemeProvider theme={smarthrTheme}>
+      {/* ライブエディタ内のコードのトランスパイルに使用するTS（容量が大きいためCDNを利用） */}
+      <Script src="https://unpkg.com/typescript@latest/lib/typescript.js" onLoad={() => setTsLoaded(true)} />
+      {tsLoaded && (
+        <LiveProvider
+          code={code}
+          language={language}
+          scope={{ ...React, ...ui, styled, css, ...scope }}
+          theme={{
+            ...themes.vsDark,
+            plain: {
+              color: CSS_COLOR.LIGHT_GREY_3,
+              backgroundColor: CSS_COLOR.TEXT_BLACK,
+            },
+          }}
+          noInline={withStyled}
+          transformCode={transformCode}
+        >
+          {/* smarthr-ui側が対応したらnoIframeの条件分岐は削除し、iframeのdocument.bodyをLiveEditorに渡してportalにする予定です。 */}
+          {!noIframe && showFrame ? (
+            <Frame
+              ref={iframeRef}
+              width="100%"
+              height={`${iframeHeight}px`}
+              style={{ border: 'none', overflow: 'hidden', display: 'block' }}
+              referrerPolicy="same-origin"
+              head={<link href="/smarthr-ui.css" rel="stylesheet" />}
+            >
+              <FrameContextConsumer>
+                {({ document }) => (
+                  <StyleSheetManager target={document?.head}>
+                    <ComponentPreview gap={gap} align={align} layout={layout}>
+                      <CssBaseLine />
+                      <LivePreview Component={React.Fragment} />
+                    </ComponentPreview>
+                  </StyleSheetManager>
+                )}
+              </FrameContextConsumer>
+            </Frame>
+          ) : (
+            <ComponentPreview gap={gap} align={align} layout={layout}>
+              <LivePreview Component={React.Fragment} />
+            </ComponentPreview>
+          )}
+          <CodeWrapper>
+            <StyledLiveEditorContainer>
+              <PreContainer>
+                <CopyButton text={code || ''} />
+                {/* @ts-ignore -- LiveEditorの型定義が正しくないようなので、エラーを無視。 https://github.com/FormidableLabs/react-live/pull/234 */}
+                <LiveEditor padding={0} />
+              </PreContainer>
+            </StyledLiveEditorContainer>
+          </CodeWrapper>
+          <LiveError />
+        </LiveProvider>
+      )}
+    </ThemeProvider>
+  )
+}
 
 const CodeWrapper = styled.div`
   position: relative;
