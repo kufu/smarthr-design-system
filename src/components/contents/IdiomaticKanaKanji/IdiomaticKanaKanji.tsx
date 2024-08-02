e3d31de100d70e150a2977a2739eff20607d520b
Aguri Otsuka <44823946+aguringo@users.noreply.github.com>
2024-08-02 17:48:02 +0900

    ディレクトリ名変更

---
+++ src/components/contents/IdiomaticKanaKanji/IdiomaticKanaKanji.tsx
@@ -0,0 +1,101 @@
+import { CSS_COLOR } from '@Constants/style'
+import { graphql, useStaticQuery } from 'gatsby'
+import { marked } from 'marked'
+import React, { FC } from 'react'
+import { Text } from 'smarthr-ui'
+import styled from 'styled-components'
+
+import { FragmentTitle } from '../../article/FragmentTitle/FragmentTitle'
+import { TextUrlToLink } from '../shared/TextUrlToLink'
+
+const query = graphql`
+  query IdiomaticKanaKanji {
+    idiomaticKanaKanji: allSdsAirtable(filter: { table: { eq: "用字一覧：漢字とひらがな" } }) {
+      edges {
+        node {
+          data {
+            name
+            description
+            discussion
+            source
+            record_id
+            order
+          }
+        }
+      }
+    }
+  }
+`
+
+export const IdiomaticKanaKanji: FC = () => {
+  const data = useStaticQuery<Queries.IdiomaticKanaKanjiQuery>(query)
+
+  const idiomaticKanaKanji = data.idiomaticKanaKanji.edges
+    .map(({ node }) => ({
+      name: node.data?.name,
+      description: marked.parse(node.data?.description || ''),
+      discussion: node.data?.discussion,
+      source: node.data?.source,
+      recordId: node.data?.record_id,
+      order: node.data?.order || Number.MAX_SAFE_INTEGER,
+    }))
+    .filter((item) => item.name)
+    .sort((x, y) => (x.order && y.order ? x.order - y.order : -1))
+
+  return (
+    <>
+      {idiomaticKanaKanji[0].recordId?.includes('MOCKDATA') && (
+        <WarningMessage>このページを正しく表示するにはAirtableのAPIキーの設定が必要です</WarningMessage>
+      )}
+      {idiomaticKanaKanji.map(({ name, description, discussion, source, recordId }, index) => {
+        const generateFragmentId = (suffixId: string) => (recordId ? `${recordId}-${suffixId}` : `${index}-${suffixId}`)
+        return (
+          <Wrapper key={index}>
+            <FragmentTitle tag="h2" id={generateFragmentId('0')}>
+              {name}
+            </FragmentTitle>
+            {description && (
+              <FragmentTitle tag="h3" id={generateFragmentId('1')}>
+                説明
+              </FragmentTitle>
+            )}
+            {description && <div dangerouslySetInnerHTML={{ __html: description }} />}
+            {discussion && (
+              <FragmentTitle tag="h3" id={generateFragmentId('2')}>
+                議事録
+              </FragmentTitle>
+            )}
+            {discussion && (
+              <StyledText as="p">
+                <TextUrlToLink text={discussion} />
+              </StyledText>
+            )}
+            {source && (
+              <FragmentTitle tag="h3" id={generateFragmentId('3')}>
+                出典
+              </FragmentTitle>
+            )}
+            {source && (
+              <StyledText as="p">
+                <TextUrlToLink text={source} />
+              </StyledText>
+            )}
+          </Wrapper>
+        )
+      })}
+    </>
+  )
+}
+
+const Wrapper = styled.div``
+const StyledText = styled(Text)`
+  white-space: pre-wrap;
+  overflow-wrap: break-word;
+`
+const WarningMessage = styled.div`
+  margin-block: 16px;
+  padding: 16px;
+  background-color: ${CSS_COLOR.CAUTION_LIGHT};
+  color: ${CSS_COLOR.CAUTION_HEAVY};
+  text-align: center;
+`
--- src/constants/airtable.ts
+++ src/constants/airtable.ts
@@ -22,8 +22,50 @@ export const AIRTABLE_CONTENTS: airtableContents[] = [
   },
   {
     pageTitle: '漢字とひらがなの使いわけ',
-    pagePath: '/products/contents/idiomatic-usage/spelling_01/',
+    pagePath: '/products/contents/idiomatic-usage/kana-kanji/',
     tableName: '用字一覧：漢字とひらがな',
+    sort: 'AIRTABLE',
+  },
+  {
+    pageTitle: 'カタカナ語の表記',
+    pagePath: '/products/contents/idiomatic-usage/katakana/',
+    tableName: '用字一覧：カタカナ語',
+    sort: 'AIRTABLE',
+  },
+  {
+    pageTitle: '数字・数えの表記',
+    pagePath: '/products/contents/idiomatic-usage/number/',
+    tableName: '用字一覧：数字・数え',
+    sort: 'AIRTABLE',
+  },
+  {
+    pageTitle: '記号の表記',
+    pagePath: '/products/contents/idiomatic-usage/mark/',
+    tableName: '用字一覧：記号',
+    sort: 'AIRTABLE',
+  },
+  {
+    pageTitle: 'SmartHRの用語',
+    pagePath: '/products/contents/idiomatic-usage/smarthr/',
+    tableName: '用語一覧：SmartHRの用語',
+    sort: 'AIRTABLE',
+  },
+  {
+    pageTitle: 'ユーザーインターフェースの呼び方',
+    pagePath: '/products/contents/idiomatic-usage/ui/',
+    tableName: '用語一覧：コンポーネント',
+    sort: 'AIRTABLE',
+  },
+  {
+    pageTitle: '操作を表す用語',
+    pagePath: '/products/contents/idiomatic-usage/action/',
+    tableName: '用語一覧：操作',
+    sort: 'AIRTABLE',
+  },
+  {
+    pageTitle: 'その他類似する用語の使いわけ',
+    pagePath: '/products/contents/idiomatic-usage/others/',
+    tableName: '用語一覧：その他',
     sort: 'AIRTABLE',
   },
 ]
