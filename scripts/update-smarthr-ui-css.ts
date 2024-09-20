722b4363a9e3f1a77741b59e16e18504d2a79d02
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-09-20 16:04:22 +0900

    chore: scriptsを追加

---
+++ scripts/update-smarthr-ui-css.ts
@@ -0,0 +1,18 @@
+import fs from 'fs'
+
+import fetch from 'node-fetch'
+
+const downloadSmarthrUiCss = async () => {
+  const url = 'https://www.unpkg.com/smarthr-ui/smarthr-ui.css'
+
+  try {
+    const response = await fetch(url)
+    const cssContent = await response.text()
+    fs.writeFileSync('static/smarthr-ui2.css', cssContent)
+    console.log('最新の smarthr-ui.css をダウンロードしました!!')
+  } catch (error) {
+    console.error('Error downloading smarthr-ui.css:', error)
+  }
+}
+
+downloadSmarthrUiCss()
