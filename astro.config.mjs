e635b76be699f04ca9fca4fb43718ee5e9853413
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-09-24 16:22:16 +0900

    chore: react, smarthr-uiを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@
 import mdx from '@astrojs/mdx';
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
 });
