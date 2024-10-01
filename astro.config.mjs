82be40b4af101af24fd275c54b76188603a2a2a0
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-10-01 17:58:07 +0900

    chore: remark-emojiを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
+import remarkEmoji from 'remark-emoji';
 import remarkCodeBlock from './src/remark/remark-code-block';
 import remarkIndexIdHeader from './src/remark/remark-index-id-header';
 
@@ -10,6 +11,6 @@ export default defineConfig({
   integrations: [mdx(), react()],
   markdown: {
     syntaxHighlight: false,
-    remarkPlugins: [remarkIndexIdHeader, remarkCodeBlock],
+    remarkPlugins: [remarkIndexIdHeader, remarkCodeBlock, remarkEmoji],
   },
 });
