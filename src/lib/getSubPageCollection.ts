a2c11220bfb2e79f81b09065edf924fb395936f9
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-10-03 16:59:16 +0900

    feat: getSubPageCollection内部でindex.mdxを除外しないように

--- src/lib/getSubPageCollection.ts
+++ src/lib/getSubPageCollection.ts
@@ -1,4 +1,4 @@
-import { getCollection } from 'astro:content';
+import { type CollectionEntry, getCollection } from 'astro:content';
 
 /**
  * 指定したディレクトリ以下のページを取得する
@@ -6,16 +6,12 @@ import { getCollection } from 'astro:content';
  * @param excludes 除外するページのパス
  * @returns ページのコレクション
  */
-export async function getSubPageCollection(basePath: string, excludes: string[] = []) {
+export async function getSubPageCollection(basePath: string, excludes: string[] = []): Promise<CollectionEntry<'articles'>[]> {
   // 先頭のスラッシュを削除
   const normalizedPath = basePath.replace(/^\//, '');
   const normalizedExcludes = excludes.map((exclude) => exclude.replace(/^\//, ''));
 
   const articles = await getCollection('articles', ({ id }) => {
-    if (id.endsWith('index.mdx')) {
-      return false;
-    }
-
     return id.startsWith(normalizedPath) && !normalizedExcludes.includes(id);
   });
 
