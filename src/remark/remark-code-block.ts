378394e682f1573daf9fb560db158dddbe8bcb6b
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-10-16 14:12:07 +0900

    refactor: コードをフォーマット

--- src/remark/remark-code-block.ts
+++ src/remark/remark-code-block.ts
@@ -4,32 +4,32 @@ import type { Code, Paragraph } from 'mdast';
 import type { Node } from 'unist';
 
 /**
- * 「```tsx editable」のようなコードブロックから、コードと言語・メタ情報を取り出すプラグイン
+ * ```tsx editable のようなコードブロックから、コードと言語・メタ情報を取り出すプラグイン
  * 参考: https://blog.mono0x.net/2023/07/10/astro-syntax-highlight-with-title/
  */
 const remarkCodeBlock = () => (tree: Node) => {
-    const visitor: Visitor<Code> = (node, index, parent) => {
-      if (!parent || index === undefined) {
-        return;
-      }
+  const visitor: Visitor<Code> = (node, index, parent) => {
+    if (!parent || index === undefined) {
+      return;
+    }
 
-      const { lang, meta, value } = node;
+    const { lang, meta, value } = node;
 
-      parent.children.splice(index, 1, {
-        type: 'paragraph',
-        data: {
-          hName: 'code-block',
-          hProperties: {
-            code: value,
-            ...(lang ? { language: lang } : {}),
-            ...(meta ? { meta } : {}),
-          },
+    parent.children.splice(index, 1, {
+      type: 'paragraph',
+      data: {
+        hName: 'code-block',
+        hProperties: {
+          code: value,
+          ...(lang ? { language: lang } : {}),
+          ...(meta ? { meta } : {}),
         },
-        children: [],
-      } as Paragraph);
-    };
-
-    visit(tree, 'code', visitor);
+      },
+      children: [],
+    } as Paragraph);
   };
+
+  visit(tree, 'code', visitor);
+};
 
 export default remarkCodeBlock;
--- src/remark/remark-index-id-header.ts
+++ src/remark/remark-index-id-header.ts
@@ -4,44 +4,44 @@ import type { Heading, ParagraphData } from 'mdast';
 import type { Node } from 'unist';
 
 const rematkIndexIdHeader = () => (tree: Node) => {
-    const indexes = {
-      1: 0,
-      2: 0,
-      3: 0,
-      4: 0,
-      5: 0,
-      6: 0,
-    };
-
-    const visitor: Visitor<Heading> = (node, index, parent) => {
-      if (!parent || index === undefined) {
-        return;
-      }
-
-      const { depth } = node;
-      if (depth > 6) return;
-
-      const tag = `h${depth}`;
-
-      // ex) `h2-0`,`h2-1`...
-      const id = `${tag}-${indexes[depth]}`;
-
-      node.data = {
-        ...node.data,
-        id,
-        htmlAttributes: {
-          id,
-        },
-        hProperties: {
-          id,
-          tag,
-        },
-      } as ParagraphData;
-
-      indexes[depth]++;
-    };
-
-    visit(tree, 'heading', visitor);
+  const indexes = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
   };
+
+  const visitor: Visitor<Heading> = (node, index, parent) => {
+    if (!parent || index === undefined) {
+      return;
+    }
+
+    const { depth } = node;
+    if (depth > 6) return;
+
+    const tag = `h${depth}`;
+
+    // ex) `h2-0`,`h2-1`...
+    const id = `${tag}-${indexes[depth]}`;
+
+    node.data = {
+      ...node.data,
+      id,
+      htmlAttributes: {
+        id,
+      },
+      hProperties: {
+        id,
+        tag,
+      },
+    } as ParagraphData;
+
+    indexes[depth]++;
+  };
+
+  visit(tree, 'heading', visitor);
+};
 
 export default rematkIndexIdHeader;
