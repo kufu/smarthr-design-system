01e932002f755938d7b55a06abbb01500db64326
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-10-23 16:06:01 +0900

    fix(scripts): 機能するよう修正

--- scripts/update-algoliasearch.ts
+++ scripts/update-algoliasearch.ts
@@ -1,8 +1,84 @@
+// Algoliaのオブジェクトを更新するスクリプト
+//
+// 安全のため、CI環境でのみ実行できるようにしています
+// ローカルで実行する場合は、CI=1 を先頭に付けて実行してください
+//
+// オプション:
+// --replace-all 指定すると全てのオブジェクトを入れ替えます (デフォルトは更新)
+
 import fs from 'node:fs';
 import path from 'node:path';
 
 import { type BatchRequest, algoliasearch } from 'algoliasearch';
 import matter from 'gray-matter';
+
+// コマンドライン引数を取得
+const args = process.argv.slice(2);
+const isReplaceAllMode = args.find((arg) => arg === '--replace-all');
+
+// src/content/articles 以下のmdxファイルを取得
+const filePaths: string[] = [];
+walkDir(path.join(import.meta.dirname, '../src/content/articles'), filePaths);
+
+// ファイルの内容を取得してAlgoliaに送信する形式に変換
+const sendData = filePaths.map((fullpath) => {
+  try {
+    const mdx = fs.readFileSync(fullpath);
+    const { data, content } = matter(mdx);
+
+    const pagePath = getPagePath(fullpath, 'articles');
+    const category = pagePath.split('/').at(0) || '';
+
+    const { title, description } = data;
+
+    return {
+      objectID: pagePath,
+      id: pagePath,
+      title,
+      category,
+      description,
+      body: content,
+      path: pagePath,
+    };
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+});
+
+if (!process.env.CI) {
+  console.warn('🚨 実行環境がCIではないようです。ローカル環境で実行する場合は、CI=1 を先頭に付けて実行してください');
+  process.exit(1);
+}
+
+const client = algoliasearch(process.env.PUBLIC_ALGOLIA_APP_ID ?? '', process.env.ALGOLIA_ADMIN_API_KEY ?? '');
+
+console.log('🚀 Algoliaにデータを送信中');
+
+if (isReplaceAllMode) {
+  // 全てのオブジェクトを入れ替える
+  client.replaceAllObjects({
+    indexName: process.env.PUBLIC_ALGOLIA_INDEX_NAME ?? '',
+    objects: sendData,
+  });
+} else {
+  const requests = sendData.map(
+    (data): BatchRequest => ({
+      action: 'updateObject',
+      body: data,
+    }),
+  );
+
+  // オブジェクトを更新する
+  await client.batch({
+    indexName: process.env.PUBLIC_ALGOLIA_INDEX_NAME ?? '',
+    batchWriteParams: {
+      requests,
+    },
+  });
+}
+
+console.log('✨ 完了');
 
 /**
  * ディレクトリを再帰的に探索して、mdxファイルのパスを取得する
@@ -46,59 +122,3 @@ function getPagePath(fullPath: string, subDir: string): `${string}/` {
 
   return `${pagePath}/`;
 }
-
-// src/content/articles 以下のmdxファイルを取得
-const filePaths: string[] = [];
-walkDir(path.join(import.meta.dirname, '../src/content/articles'), filePaths);
-
-// ファイルの内容を取得してAlgoliaに送信する形式に変換
-const sendData = filePaths.map((fullpath) => {
-  try {
-    const mdx = fs.readFileSync(fullpath);
-    const { data, content } = matter(mdx);
-
-    const pagePath = getPagePath(fullpath, 'articles');
-    const category = pagePath.split('/').at(0) || '';
-
-    const { title, description } = data;
-
-    return {
-      id: pagePath,
-      title,
-      category,
-      description,
-      body: content,
-      path: pagePath,
-    };
-  } catch (e) {
-    console.error(e);
-    throw e;
-  }
-});
-
-if (!process.env.CI) {
-  console.log('🚨 実行環境がCIではないようです');
-  process.exit(1);
-}
-
-// Algoliaにデータを送信
-const client = algoliasearch(process.env.PUBLIC_ALGOLIA_APP_ID ?? '', process.env.PUBLIC_ALGOLIA_SEARCH_API_KEY ?? '');
-
-const requests = sendData.map(
-  (data): BatchRequest => ({
-    action: 'updateObject',
-    body: data,
-  }),
-);
-
-console.log('🚀 Algoliaにデータを送信中');
-
-// TODO: 動作確認
-await client.batch({
-  indexName: process.env.PUBLIC_ALGOLIA_INDEX_NAME ?? '',
-  batchWriteParams: {
-    requests,
-  },
-});
-
-console.log('✨ 完了');
