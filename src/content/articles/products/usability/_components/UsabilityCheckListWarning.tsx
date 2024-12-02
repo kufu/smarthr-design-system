f389da91c576c4c8b93bc6565c49c0ed0bf01e41
so <hiroaki.so@smarthr.co.jp>
2024-12-02 12:17:01 +0900

    Revert "Merge remote-tracking branch 'origin/reorder-design-pattern' into renew_design-review"
    This reverts commit 8895c30c44392b2bcb242e4ac5fc093e55122a6d, reversing
    changes made to c665c32a8d0138f4ab2aba16bafd53259f673a83.

--- src/content/articles/products/usability/_components/UsabilityCheckListWarning.tsx
+++ src/content/articles/products/usability/_components/UsabilityCheckListWarning.tsx
@@ -6,7 +6,7 @@ export default function UsabilityCheckListWarning() {
       <WarningIcon
         text={
           <span>
-            ライティングの「No.19 SmartHRの基本的な概念（用語）に合わせた命名をしているか」および「No.20
+            コンテンツの「No.19 SmartHRの基本的な概念（用語）に合わせた命名をしているか」および「No.20
             操作の導線やアクション名が基準に沿っているか」のチェックは任意とします。（現時点では確認作業が大きくなりすぎる可能性があるため）
           </span>
         }
